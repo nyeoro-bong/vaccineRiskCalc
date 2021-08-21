aac536661a6e5492b23d8db75f9a6253b849d9f3
nyeoro-bong <68091561+nyeoro-bong@users.noreply.github.com>
2021-08-21 15:48:09 +0900

    rev71

--- vRiskCalc.js
+++ vRiskCalc.js
@@ -18,7 +18,7 @@ getCSV = () => {
   dataD.open('GET','demography.csv',true);
   dataD.send();
   
-  lines = dataD.responseText.toString.split('n');
+  lines = respnse.toString.split('n');
 
   // 各行ごとにカンマで区切った文字列を要素とした二次元配列を生成
   for (let i = 0; i < lines.length ; ++i) {
