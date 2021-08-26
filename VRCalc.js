2a2d9897ee997ab4be2227e6bee0e0bf02634803
nyeoro-bong <68091561+nyeoro-bong@users.noreply.github.com>
2021-08-26 16:47:44 +0900

    rev6

--- VRCalc.js
+++ VRCalc.js
@@ -18,6 +18,7 @@ getCSV = () => {
     
     // 各行ごとにカンマで区切った文字列を要素とした二次元配列を生成
     for (let i = 0; i < lines.length ; ++i) {
+      datasD[i] = lines[i].split(',');
     }
   });
   csvData.open('GET','demography.csv',true);
