754a944b6202d15d4d475a64298f9c50a005d658
nyeoro-bong <68091561+nyeoro-bong@users.noreply.github.com>
2021-08-26 14:27:42 +0900

    Add via upload 0826

---
+++ VRCalc.js
@@ -0,0 +1,45 @@
+let userNameImput = document.getElementById(`userName`);
+let userAgeImput = document.getElementById(`userAge`);
+let userVacImput = document.getElementById(`userVac`);
+let riskCalcButton = document.getElementById(`riskCalc`);
+let outputDivided = document.getElementById(`output-area`);
+let resultDivided = document.getElementById(`result-area`);
+let infoDivided = document.getElementById(`info-area`);
+let tweetDivided = document.getElementById(`tweet-area`);
+
+getCSV = () => {
+  let datasD = [];
+  let lines = [];
+  let csvData = new XMLHttpRequest();
+  csvData.addEventListener('load', (event) => {
+    const response = event.target.responseText;
+    outputDivided.innerHTML = response;
+    lines = response.split('\n');
+    
+    // 各行ごとにカンマで区切った文字列を要素とした二次元配列を生成
+    for (let i = 0; i < lines.length ; ++i) {
+      datasD[i] = lines[i].split(',');
+    }
+  });
+  csvData.open('GET','demography.csv',true);
+  csvData.send();
+  return datasD;
+}
+
+function riskCalcD() {
+      console.log(datasD);
+      ageG = datasD[2][3];
+}
+
+getCSV();
+
+riskCalcButton.onclick = () => {
+  let uName = userNameImput.value;
+  let uAge = userAgeImput.value;
+  let uVac = userVacImput.value;
+
+  riskCalcD();
+
+  resultDivided.innerHTML = `年代は${ageG}です。`;
+
+}
