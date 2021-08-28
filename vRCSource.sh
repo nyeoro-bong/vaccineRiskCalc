ae9aea6da35664d650c23e9aee7cabe069479dca
nyeoro-bong <68091561+nyeoro-bong@users.noreply.github.com>
2021-08-28 21:11:16 +0900

    remove

--- vRCSource.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-filename1="demography.csv"
-filename2="eRNumber.csv"
-curl -s -o $filename1 -H "User-Agent: CrawlBot; nyolobong@gmail.com" https://toyokeizai.net/sp/visual/tko/covid19/csv/demography.csv
-echo "保存しました: $filename1"
-curl -s -o $filename2 -H "User-Agent: CrawlBot; nyolobong@gmail.com" https://toyokeizai.net/sp/visual/tko/covid19/csv/effective_reproduction_number.csv
-echo "保存しました: $filename2"
