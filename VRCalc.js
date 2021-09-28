5fd0ae69851bbda90632fcc66d375e0f2c8641bc
nyeoro-bong <68091561+nyeoro-bong@users.noreply.github.com>
2021-09-28 21:00:20 +0900

    update092801

--- VRCalc.js
+++ VRCalc.js
@@ -197,13 +197,13 @@ riskCalcButton.onclick = () => {
   outputDivided.innerHTML = `
   注① 重症者：エクモ等の人工呼吸器を必要とする状態の者<br>
   注② 交通事故死亡率：計算式＝交通事故死者数(事故原因で30日以内に死亡した者)${deathTrafficAccident2020} ÷ 人身事故発生件数${trafficAccident2020}（年度：2020、単位：%）<br>
-  (参考）９月１９日報道発表資料時点での全世代感染致死率:1.11%　＝（死者数:17156 + 重症:1496）/ 陽性者数:1673144 <br>
+  (参考）９月２６日報道発表資料時点での全世代感染致死率:1.10%　＝（死者数:17453 + 重症:1133）/ 陽性者数:1692387 <br>
   `;
   
   infoDivided.innerHTML = `【オープンデータ掲載ソースを確認する】<br>
  <a href="https://www.e-stat.go.jp/stat-search/files?page=1&layout=datalist&toukei=00130002&tstat=000001032793&cycle=7&year=20200&month=0"> 令和２年中の交通事故死者について | 警察庁交通局交通企画課</a><br>
  <a href="https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/vaccine_yuukousei_anzensei.html"> 新型コロナワクチンの有効性・安全性について | 厚生労働省</a><br>
- <a href="https://www.mhlw.go.jp/stf/newpage_21185.html"> 新型コロナウイルス感染症の現在の状況と厚生労働省の対応について（令和３年９月1９日版） | 厚生労働省</a><br>
+ <a href="https://www.mhlw.go.jp/stf/newpage_21272.html"> 新型コロナウイルス感染症の現在の状況と厚生労働省の対応について（令和３年９月26日版） | 厚生労働省</a><br>
  <a href="https://toyokeizai.net/sp/visual/tko/covid19/"> 『東洋経済オンライン「新型コロナウイルス 国内感染の状況」制作：荻原和樹』（CSVデータ参照元）</a><br><br>
  `;
 
