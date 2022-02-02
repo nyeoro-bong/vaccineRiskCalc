462af00b370cc95d78e5a3adfa68280344679ffd
nyeoro-bong <68091561+nyeoro-bong@users.noreply.github.com>
2022-02-02 21:07:17 +0900

    Herokuの環境変数を利用できるように修正7

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,11 @@ const express = require('express');
 const router = express.Router();
 const Dataset = require('../models/dataset');
 
+const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc');
+const timezone = require('dayjs/plugin/timezone');
+dayjs.extend(utc);
+dayjs.extend(timezone);
 /* GET home page. */
 router.get('/', function (req, res, next) {
   const title = 'covidリスク確認くん';
@@ -13,6 +18,9 @@ router.get('/', function (req, res, next) {
       },
       order: [['updatedAt', 'DESC']]
     }).then(datasets => {
+      datasets.forEach(dataset => {
+        dataset.formattedUpdatedAt = dayjs(dataset.updatedAt).tz('Asia/Tokyo').format('YYYY/MM/DD HH:mm');
+      });
       res.render('index', {
         title: title,
         user: req.user,
